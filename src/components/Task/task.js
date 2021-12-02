81a66fe1a37746c2e10cf3f21154b66adc2a4e62
Diego Zambrana <contacto@diegozambrana.com>
2021-12-01 22:31:47 -0400

    update

--- src/components/Task/task.js
+++ src/components/Task/task.js
@@ -2,16 +2,26 @@ import React from 'react';
 import './task.css';
 import {Arrow} from '../icons/arrows';
 
-const Task = ({item}) => (
-  <div className="task">
-    <div style={{marginRight: 16}}>
-      <Arrow direction={'down'} pointer/>
+const Task = ({item}) => {
+  const [showSteps, setShowSteps] = React.useState(false);
+
+  return (
+    <div className="task">
+      <div style={{marginRight: 16}}>
+        <Arrow
+          direction={showSteps ? 'up' : 'down'}
+          pointer
+          onClickArrow={() => {
+            setShowSteps((show) => setShowSteps(!show));
+          }}
+        />
+      </div>
+      <p className="task-text">{item.name}</p>
+      <div className="task-check">
+        <input type="checkbox" checked={item.completed}/>
+      </div>
     </div>
-    <p className="task-text">{item.name}</p>
-    <div className="task-check">
-      <input type="checkbox" checked={item.completed}/>
-    </div>
-  </div>
-);
+  )
+};
 
 export { Task };
--- src/components/icons/arrows.js
+++ src/components/icons/arrows.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './arrows.css';
 
-export const Arrow = ({direction, pointer}) => (
+export const Arrow = ({direction, pointer, onClickArrow}) => (
     <div
         className={`arrow ${
             direction === 'up' ? 'arrow-up' : 'arrow-down'
         } ${pointer ? 'pointer' : ''}`}
+        onClick={() => onClickArrow ? onClickArrow() : null}
     />
 )
